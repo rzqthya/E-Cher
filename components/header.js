68a4c7130092d6b5834c06db288d1f663bc10684
Angelaczr <angelacaezaria@gmail.com>
2023-11-03 21:38:17 +0700

    header

---
+++ components/header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { TouchableOpacity } from "react-native";
+import { Box, Heading, HStack } from "native-base";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+const CustomHeader = ({ title, navigation }) => {
+  return (
+    <Box p={4} bg="white" flexDirection="row" alignItems="center">
+      <HStack alignItems="center">
+        <TouchableOpacity
+          activeOpacity={0.5}
+          onPress={() => navigation.goBack()}
+        >
+          <Box mr={"3"}>
+            <Ionicons name="arrow-back-outline" size={20} color="#F82F2D" />
+          </Box>
+        </TouchableOpacity>
+        <Heading color="#F82F2D" fontSize={"16"}>{title}</Heading>
+      </HStack>
+    </Box>
+  );
+};
+
+export default CustomHeader;

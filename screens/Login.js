66b39229b8667999b6a3c469f9faf862150f0a5c
aidarifdatul <aidahasna42@gmail.com>
2023-11-07 17:34:18 +0700

    update validatio

--- screens/Login.js
+++ screens/Login.js
@@ -2,18 +2,56 @@ import React, { useState } from 'react';
 import { View, Text, Image } from 'react-native';
 import { NativeBaseProvider, Input, Button } from 'native-base';
 
-
-const Login = ({ navigation }) => {  // navigation diisi adalah "props" yang yang diterima dari react navigator yang digunakan untuk mengatur "useState"
+const Login = ({ navigation }) => { // navigation diisi adalah "props" yang yang diterima dari react navigator yang digunakan untuk mengatur "useState"
   //code dibawah ini merupakan State
-  //penggunaan useState untuk membuat state lokal dalam komponen Login yang menyimpan nilai email dan phoneNumber
+  //penggunaan useState untuk membuat state lokal dalam komponen Login yang menyimpan
   const [email, setEmail] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [phoneNumberError, setPhoneNumberError] = useState('');
+
+  const dummyUserData = [
+    { email: 'aida@gmail.com', phoneNumber: '1234567890' },
+  ];
 
   const handleSignUp = () => {
     navigation.navigate('SignUp');
   };
 
   const handleLogin = () => {
+    // error message
+    setEmailError('');
+    setPhoneNumberError('');
+
+    let hasError = false;
+
+    // jika email tidak diisi akan muncul error
+    if (!email) {
+      setEmailError('Email is required');
+      hasError = true;
+    }
+
+    // jika phone number tidak diisi akan muncul error
+    if (!phoneNumber) {
+      setPhoneNumberError('Phone Number is required');
+      hasError = true;
+    }
+
+    // Jika terjadi kesalahan maka proses login berhenti
+    if (hasError) {
+      return;
+    }
+
+    // Check apakah email dan phone number yang dimasukan sudah sesuai dengan data dummy
+    const user = dummyUserData.find((user) => user.email === email && user.phoneNumber === phoneNumber); //user.email dan user.phoneNumber diambil dari value
+
+    // Kondisi ketika email atau phone number yang dimasukkan salah
+    if (!user) {
+      setEmailError('Invalid email or phone number');
+      return;
+    }
+
+    // Kondisi apabila data yang dimasukkan benar
     navigation.navigate('Home');
   };
 
@@ -26,24 +64,32 @@ const Login = ({ navigation }) => {  // navigation diisi adalah "props" yang yan
         </View>
         <View width="80%" marginBottom={10}>
           {/* Inputan Email */}
-          <Text marginBottom={10}>Masukkan Email:</Text>
+          <Text marginBottom={10}>Masukkan Email</Text>
           <Input
             placeholder="Email"
             value={email}
-            onChangeText={(text) => setEmail(text)}
+            onChangeText={(text) => {
+              setEmail(text);
+              setEmailError('');
+            }}
             style={{ marginBottom: 10 }}
           />
+          <Text style={{ color: 'red', marginBottom: 5 }}>{emailError}</Text>
 
           {/* Inputan No.Telpon */}
           <Text marginBottom={20} marginTop={20}>
-            Masukkan No. Telpon:
+            Masukkan No. Telpon
           </Text>
           <Input
             placeholder="Phone Number"
             value={phoneNumber}
-            onChangeText={(text) => setPhoneNumber(text)}
+            onChangeText={(text) => {
+              setPhoneNumber(text);
+              setPhoneNumberError('');
+            }}
             style={{ marginBottom: 10 }}
           />
+          <Text style={{ color: 'red', marginBottom: 5 }}>{phoneNumberError}</Text>
         </View>
         <Button
           onPress={handleLogin}

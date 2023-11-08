8bf08869792df15e5ff7af914a9d0c48d1d499fd
aisyahnabila <aisyahnabila2.an@gmail.com>
2023-11-08 23:59:15 +0700

    elevation

--- components/hi_profile.js
+++ components/hi_profile.js
@@ -1,8 +1,10 @@
-import { SafeAreaView, TouchableOpacity, StatusBar } from 'react-native'
-import { Avatar, Box, HStack, Text } from 'native-base'
-import React from 'react'
+import { SafeAreaView, TouchableOpacity, StatusBar } from 'react-native';
+import { Avatar, Box, HStack, Text } from 'native-base';
+import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 
 const Hi_profile = ({ title }) => {
+    const navigation = useNavigation();
     return (
         <SafeAreaView>
             <StatusBar barStyle="light" backgroundColor={'#7F7F7F'} alignItems={'center'} />
@@ -13,7 +15,8 @@ const Hi_profile = ({ title }) => {
                     paddingHorizontal: 15,
                     paddingVertical: 15,
                     borderRadius: 10,
-                }}>
+                    elevation: 2,
+                }} onPress={() => navigation.navigate('Profile')}>
                     <HStack justifyContent={'center'} alignItems={'center'} space={3}>
                         <Avatar bg="green.500" source={{
                             uri: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"

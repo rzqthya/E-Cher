7d967e24a6c8aaee13474594cb57aa77237ee133
aidarifdatul <aidahasna42@gmail.com>
2023-11-08 20:40:36 +0700

    Inisialisasi Awal

---
+++ screens/edit-profile.js
@@ -0,0 +1,14 @@
+import { Heading, Center } from "native-base";
+
+
+const EditProfile = () => {
+  return (
+    <>
+      <Center flex={1} bg="#F0F2F7">
+        <Heading>Edit Profile</Heading>
+      </Center>
+    </>
+  );
+};
+
+export default EditProfile;

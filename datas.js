9cc342e5bf46a5f30f49c58f9a1112c2008ceea4
aisyahnabila <aisyahnabila2.an@gmail.com>
2023-11-09 00:39:38 +0700

    + home code, - assets

---
+++ datas.js
@@ -0,0 +1,103 @@
+const city =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida, orci nec feugiat commodo, ipsum orci consequat nulla, non sodales sem lorem et ante. Etiam dignissim tempor elit, nec feugiat ante suscipit eget. Sed imperdiet diam at ultrices viverra. Duis malesuada mattis tortor, ac ultrices elit euismod a.\n\nSuspendisse tincidunt eleifend volutpat. Phasellus vehicula sapien eu aliquam dignissim. Duis diam lorem, porta id nisi a, interdum rutrum quam. Suspendisse tincidunt euismod nunc. Etiam interdum, sem tincidunt sodales dignissim, orci ligula hendrerit dolor, ac suscipit neque eros sit amet magna. Sed gravida porttitor justo in venenatis. Maecenas dolor odio, semper eu velit pharetra, euismod placerat lectus. Curabitur nec aliquam mi. Aliquam suscipit iaculis vestibulum. Integer nibh ante, dignissim sed convallis eget, elementum ut nunc. Fusce non pharetra enim. Etiam ut justo est. Morbi commodo bibendum lorem a condimentum. Nulla ut magna tempor neque fringilla auctor. Ut aliquam ipsum urna, sodales ultricies ipsum pulvinar ac.\n\nNullam ultrices egestas dapibus. Vivamus sodales, nisl vel lobortis vestibulum, elit libero venenatis mauris, ut efficitur augue turpis ac lorem. Duis eu hendrerit ligula. Mauris auctor, purus ut ornare condimentum, ligula nisi egestas erat, eu fermentum libero massa tincidunt turpis. Suspendisse porta luctus elit eu pellentesque. Vivamus feugiat pellentesque hendrerit. Duis eu vulputate urna, quis bibendum nulla.\n\nPhasellus et faucibus elit, mollis accumsan arcu. Fusce scelerisque orci purus, vel scelerisque nisl viverra vitae. Nulla non lectus in lacus mollis consequat. Aenean consequat luctus tristique. Nunc imperdiet condimentum cursus. Praesent vel fermentum est, id pulvinar est. Nunc dolor velit, commodo non vehicula at, tincidunt lobortis mauris. Curabitur laoreet ipsum ut erat interdum ornare.";
+
+const datas = [
+    {
+        id: 1,
+        date: "29 Oktober 2023",
+        title:
+            "Burger King Cash Back Hingga 50%",
+        desc: "All Outlet Burger King",
+        image:
+            require('./assets/voucher/BurgerKing_promo1.jpg'),
+        city: "Semarang",
+    },
+    {
+        id: 2,
+        date: "28 Februari 2023",
+        title: "Voucher Diskon Rp 15.000",
+        desc: "Khusus Outlet Jawa Tengah",
+        image:
+            require('./assets/voucher/BurgerKing_Promo2.jpg'),
+        city: "DIY Yogyakarta",
+    },
+    {
+        id: 3,
+        date: "31 November 2023",
+        title:
+            "Voucher Diskon Rp 20.000",
+        desc: "Khusus Siswa dan Mahasiswa",
+        image:
+            require('./assets/voucher/Chatime1.jpg'),
+        city: "Solo",
+    },
+    {
+        id: 4,
+        date: "16 Mei 2023",
+        title: "THR Dari Kenangan Beli 3 Hanya 48 Ribu",
+        desc: "Khusus Siswa dan Mahasiswa",
+        image:
+            require('./assets/voucher/Chatime2.jpg'),
+        city: "Boyolali",
+    },
+    {
+        id: 5,
+        date: "16 Mei 2023",
+        title:
+            "Cashback 60%",
+        desc: "Untuk Pengguna GoPay",
+        image:
+            require('./assets/voucher/dcost-promo.jpg'),
+        city: "Banyumas",
+    },
+    {
+        id: 6,
+        date: "10 Desember 2023",
+        title:
+            "Diskon Nasi Lemak McD",
+        desc: "Promo Akhir Tahun",
+        image:
+            require('./assets/voucher/Gildak_1.jpg'),
+        city: "Blora",
+    },
+    {
+        id: 7,
+        date: "Setiap Hari 22.00",
+        title: "Diskon Kopi",
+        desc: "All Days",
+        image:
+            require('./assets/voucher/kopikenangan_1.jpg'),
+        city: "Brebes",
+    },
+    {
+        id: 8,
+        date: "31 Oktober 2023",
+        title:
+            "Promo Oktober Xtra Puas",
+        desc: "Khusus siswa dan mahasiswa",
+        image:
+            require('./assets/voucher/kopikenangan_2.jpg'),
+        city: "Cilacap",
+    },
+    {
+        id: 9,
+        date: "7 Juli 2023",
+        title: "Voucher Belanja Chatime",
+        desc: "Promo 7.7 untuk pembelian melalui Ovo",
+        image:
+            require('./assets/voucher/Martabakku1.jpg'),
+        city: "Demak",
+    },
+    {
+        id: 10,
+        date: "20 Desember 2023",
+        title:
+            "Cashback 35% MartabakKu",
+        desc: "Promo Akhir Tahun",
+        image:
+            require('./assets/voucher/McD1.jpg'),
+        city: "Kebumen",
+    },
+];
+
+export default datas;

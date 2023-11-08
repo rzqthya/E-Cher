534e6581140364bf379dc249aa6026a6b0ee0f16
aisyahnabila <aisyahnabila2.an@gmail.com>
2023-11-09 02:11:07 +0700

    + detail voucher

--- screens/detailvoucher.js
+++ screens/detailvoucher.js
@@ -1,14 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 import React from 'react'
+import { Header } from '../components'
 
-const DetailVoucher = () => {
+const DetailVoucher = ({ route }) => {
+    const params = route.params.item;
     return (
         <View>
+            <Header title={'Detail Voucher'} />
             <Text>detailvoucher</Text>
+            <Text textAlign={"center"}>{params.title}</Text>
+
         </View>
     )
 }
 
 export default DetailVoucher
 
-const styles = StyleSheet.create({})

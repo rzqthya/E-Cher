4c0cf634ae42e82e2d9778257a0e12e1698ad35b
aidarifdatul <aidahasna42@gmail.com>
2023-11-09 09:49:45 +0700

    update edit profile

--- screens/edit-profile.js
+++ screens/edit-profile.js
@@ -1,13 +1,69 @@
-import { Heading, Center } from "native-base";
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
+import { NativeBaseProvider, Input, Button } from 'native-base';
+
+const EditProfile = ({ navigation }) => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
 
-const EditProfile = () => {
+  const handleSave = () => {
+    navigation.navigate('Profile');
+  };
+
   return (
-    <>
-      <Center flex={1} bg="#F0F2F7">
-        <Heading>Edit Profile</Heading>
-      </Center>
-    </>
+    <NativeBaseProvider>
+      <View flex={1} justifyContent="center" alignItems="center" marginBottom={20}>
+        <View width="80%" marginBottom={100}>
+          {/* Ubah Username */}
+          <Text marginBottom={10}>
+            Ubah Username
+          </Text>
+          <Input
+            placeholder="Username"
+            value={username}
+            onChangeText={(text) => {
+              setUsername(text);
+            }}
+          />
+
+          {/* Ubah Email */}
+          <Text marginBottom={10} marginTop={20}>
+              Ubah Email
+          </Text>
+          <Input
+            placeholder="Email"
+            value={email}
+            onChangeText={(text) => {
+              setEmail(text);
+            }}
+          />
+
+          {/* Ubah Password */}
+          <Text marginBottom={20} marginTop={20}>
+              Ubah Password
+          </Text>
+          <Input
+            placeholder="Password"
+            value={password}
+            onChangeText={(text) => {
+              setPassword(text);
+            }}
+          />
+        </View>
+
+        {/* Button Handle */}
+        <Button
+          onPress={handleSave}
+          marginTop={10}
+          marginBottom={10}
+          style={{ backgroundColor: '#D32324', width: '80%', borderRadius: 12 }}
+        >
+          Save
+        </Button>
+      </View>
+    </NativeBaseProvider>
   );
 };
 

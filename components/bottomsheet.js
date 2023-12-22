b7b8ad31e60bf33394220de706124fc4d8eafe0d
aisyahnabila <aisyahnabila2.an@gmail.com>
2023-12-22 19:59:49 +0700

    +Datepicker design fix

--- components/bottomsheet.js
+++ components/bottomsheet.js
@@ -1,13 +1,15 @@
-import React, { useRef, useMemo, useCallback } from 'react';
+import React, { useRef, useMemo, useCallback, useState } from 'react';
 import { FlatList, Box, Text } from 'native-base';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, Pressable, Platform } from 'react-native';
 import BottomSheet, { BottomSheetBackdrop } from '@gorhom/bottom-sheet';
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { TextInput } from 'react-native-gesture-handler';
 
 const BottomSheetComponent = ({ isBottomSheetOpen, setIsBottomSheetOpen, kategori }) => {
     // ref
     const bottomSheetRef = useRef(null);
     // variables
-    const snapPoints = useMemo(() => ['25%', '50%', '90%'], []);
+    const snapPoints = useMemo(() => ['50%', '90%'], []);
 
     const renderBackdrop = useCallback((props) => (
         <BottomSheetBackdrop
@@ -17,10 +19,37 @@ const BottomSheetComponent = ({ isBottomSheetOpen, setIsBottomSheetOpen, kategor
             onPress={() => setIsBottomSheetOpen(false)}
         />
     ), []);
+
+    // DatePicker
+    const [date, setDate] = useState(new Date());
+    const [showPicker, setShowPicker] = useState(false);
+    const toggleDatepicker = () => {
+        setShowPicker(!showPicker);
+    };
+    const [dateOfExp, setDateOfExp] = useState("");
+    const onChange = ({ type }, selectedDate) => {
+        if (type == 'set') {
+            const currentDate = selectedDate;
+            setDate(currentDate);
+            if (Platform.OS === 'android') {
+                toggleDatepicker();
+                setDateOfExp(formatDate(currentDate));
+            }
+        } else {
+            toggleDatepicker();
+        }
+    };
+    const formatDate = (rawDate) => {
+        let date = new Date(rawDate);
+        let year = date.getFullYear();
+        let month = date.getMonth() + 1;
+        let day = date.getDate();
+        return `${day}-${month}-${year}`;
+    };
     return (
         <BottomSheet
             ref={bottomSheetRef}
-            index={1}
+            index={0}
             snapPoints={snapPoints}
             enablePanDownToClose={true}
             backdropComponent={renderBackdrop}
@@ -47,8 +76,33 @@ const BottomSheetComponent = ({ isBottomSheetOpen, setIsBottomSheetOpen, kategor
                         }}>
                             <Text>{item.nama}</Text>
                         </TouchableOpacity>)} />
+                <Box>
+                    <Text fontSize={18} fontWeight={600}>Masa Berlaku</Text>
+                    {showPicker && (<DateTimePicker
+                        mode='date'
+                        display='spinner'
+                        value={date}
+                        onChange={onChange}
+                    />)}
+                    {!showPicker && (
+                        <Pressable onPress={toggleDatepicker}>
+
+                            <TextInput style={{
+                                height: 40,
+                                margin: 12,
+                                borderWidth: 1,
+                                borderColor: '#A9A9A9',
+                                borderRadius: 6,
+                                padding: 10
+                            }} placeholder='Aug 21 2024'
+                                value={dateOfExp}
+                                onChangeText={setDateOfExp}
+                                placeholderTextColor={"#A9A9A9"}
+                                editable={false} />
+                        </Pressable>)}
+                </Box>
             </Box>
-        </BottomSheet>
+        </BottomSheet >
     );
 };
 
--- datas.js
+++ datas.js
@@ -1,21 +1,26 @@
 const datas = [
     {
+        id: 1,
         overview: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         sk: '1. Voucher ini berlaku sesuai dengan tanggal yang tertera pada voucher \n2. Hanya bisa digunakan pada merchant yang sudah dipilih kotanya.'
     },
     {
+        id: 2,
         overview: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         sk: '1. Voucher ini berlaku sesuai dengan tanggal yang tertera pada voucher \n2. Hanya bisa digunakan pada merchant yang sudah dipilih kotanya.'
     },
     {
+        id: 3,
         overview: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         sk: '1. Voucher ini berlaku sesuai dengan tanggal yang tertera pada voucher \n2. Hanya bisa digunakan pada merchant yang sudah dipilih kotanya.'
     },
     {
+        id: 4,
         overview: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         sk: '1. Voucher ini berlaku sesuai dengan tanggal yang tertera pada voucher \n2. Hanya bisa digunakan pada merchant yang sudah dipilih kotanya.'
     },
     {
+        id: 5,
         overview: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         sk: '1. Voucher ini berlaku sesuai dengan tanggal yang tertera pada voucher \n2. Hanya bisa digunakan pada merchant yang sudah dipilih kotanya.'
     },

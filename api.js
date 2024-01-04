aab4973f7874fd44017a18fbf1c611d40ee63528
aidarifdatul <aidahasna42@gmail.com>
2024-01-04 20:57:16 +0700

    update

--- api.js
+++ api.js
@@ -1,28 +1,13 @@
+// api.js
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.118.127:8000', 
+  baseURL: 'http://192.168.242.24:8000',
   headers: {
     'Content-Type': 'application/json',
-  },
+    
+  }
 });
 
+
 export default api;
-
-
-// export const api = {
-//     register: async (user) => {
-//         const response = await fetch(`${BASE_URL}/register`, {
-//             method: 'POST',
-//             headers: headers,
-//             body: JSON.stringify(user),
-//         });
-
-//         if (!response.ok) {
-//             const data = await response.json();
-//             throw new Error(data.message);
-//         }
-
-//         return response.json();
-//     },
-// };
